844ab8407d9236e1793a5ec9242273439220b3e2
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2023-06-22 08:55:54 -0600

    extensions moved out of WYUP

---
+++ Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using Verse;
+
+namespace CodeOptimist
+{
+    public static class Extensions
+    {
+        // `Resolve()` supports colored text e.g. `<Threat>text</Threat>`
+        public static string ModTranslate(this string key, params NamedArgument[] args) => $"{Gui.modId}_{key}".Translate(args).Resolve();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Squared(this float val) => val * val;
+    }
+}
